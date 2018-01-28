72e7b05c70e46d34d57e815f02c3f5407f36a540
Mahmudul Hasan <mhasan0047@gmail.com>
2018-01-28 13:00:32 -0500

    made it refresh successfully

--- Bitcoin_GDAX.py
+++ Bitcoin_GDAX.py
@@ -1,31 +1,37 @@
-
 #Working
 import requests
 import json
-
-#getting current price:
-url = 'https://api.gdax.com/products/BTC-USD/trades'
-res = requests.get(url)
-json_res = json.loads(res.text)
-currDollarBit = json_res[0]['price'] #current dollars per bitcoin
-currDollarBit = float(currDollarBit)    #turning to float to make calculations
+import time
 
 #what i have:
 myBTC = 0.008385640 
 myDollar = 99 					#what i put in
 
-#calculate:
-# Current: 1BTC = currentDollar
-# Mine:    0.008385640BTC = $99 
-currDollar = myBTC * currDollarBit
-print("Current Dollar/Bitcoin: " + str(currDollar))
 
-profit = currDollar - 99 
+def calcProfits():
+	#getting current price:
+	url = 'https://api.gdax.com/products/BTC-USD/trades'
+	res = requests.get(url)
+	json_res = json.loads(res.text) 		#"json_res" got a ton of stuff. the price is in "json_res[0]""
+	currDollarBit = json_res[0]['price']    #current dollars per bitcoin. 'price' is the location in the 0th index of "jason_res"
+	currDollarBit = float(currDollarBit)    #turning to float to make calculations
 
-if (profit < 0):  	#if im losing money
-	print("loss: $" + str(profit))
-elif (profit > 0):  #if im gaining money
-	print("profit: $"+  str(profit))
+
+	#calculate:
+	# Current: 1BTC = currentDollar
+	# Bought:  0.008385640BTC = $99 
+	currDollar = myBTC * currDollarBit
+	#print("Current Dollar/Bitcoin: " + str(currDollar))
+
+	profit = currDollar - 99 
+	if (profit < 0):  	#if im losing money
+		print("loss: $" + str(profit))
+	elif (profit > 0):  #if im gaining money
+		print("profit: $"+  str(profit))
+	time.sleep(.6)	#program take .5s to execute. so making it refresh at .6s
+
+while True:
+	calcProfits()
 
 
 
