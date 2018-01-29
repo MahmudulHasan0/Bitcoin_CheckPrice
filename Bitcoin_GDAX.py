575bece5fc59ca891998e6af5e1f8d1a54b6da9d
Mahmudul Hasan <mhasan0047@gmail.com>
2018-01-28 20:26:21 -0500

    added my actual money

--- Bitcoin_GDAX.py
+++ Bitcoin_GDAX.py
@@ -22,20 +22,20 @@ def calcProfits():
 	json_res = json.loads(res.text) 		#"json_res" got a ton of stuff. the price is in "json_res[0]""
 	currDollarBit = json_res[0]['price']    #current dollars per bitcoin. 'price' is the location in the 0th index of "jason_res"
 	currDollarBit = float(currDollarBit)    #turning to float to make calculations
-	currDollar = myBTC * currDollarBit
+	currDollar = round(myBTC * currDollarBit, 3)
 	profit = round(currDollar - 99, 3)
 
 #Calculate Profits:
 	if (profit == 0):  #if im gaining money
-		sys.stdout.write("base: $" +  str(abs(profit)) + "      ")
+		sys.stdout.write("base: $" +  str(abs(currDollar)) + "   $" +  str(abs(profit)) + "   |   ")
 	elif (profit > 0):  #if im gaining money
-		sys.stdout.write("gain: +$" +  str(abs(profit)) + "      ")
+		sys.stdout.write("gain: +$" +  str(abs(currDollar)) + "   +$" + str(abs(profit)) + "   |   ")
 	elif (profit < 0):  	#if im losing money
-		sys.stdout.write("loss: -$" + str(abs(profit)) + "      ")
+		sys.stdout.write("loss: -$" +  str(abs(currDollar)) + "   -$" + str(abs(profit)) + "   |   ")
 #Calculate My Current Profit:
 	
 #Calculate Percent Change/slope
-	sys.stdout.write("BTC/USD: " + str(currDollarBit) + "      ")  #current BTC/USD
+	sys.stdout.write("BTC/USD: " + str(currDollarBit) + "   |   ")  #current BTC/USD
 	if (firstTimeRunning == True):
 		firstTimeRunning = False
 		profit_past = profit
@@ -64,7 +64,6 @@ def calcProfits():
 		count = 0;
 		firstTimeRunning = True
 	time.sleep(1)	#program take .5s to execute. so making it refresh at .9s
-#ok
 while True:
 	calcProfits()
 	
