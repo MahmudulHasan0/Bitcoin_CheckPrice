857b9ef88b42a5a685ee2c9052fa6e558cecaf0f
Mahmudul Hasan <mhasan0047@gmail.com>
2018-01-28 23:03:34 -0500

    added percent total gaim/loss

--- Bitcoin_GDAX.py
+++ Bitcoin_GDAX.py
@@ -3,6 +3,7 @@ import requests
 import json
 import time		#to refresh scans, repeat function
 import sys		#to print in one line
+
 #MY BITCOIN:
 myBTC = 0.012716460
 myDollar = 148					
@@ -10,7 +11,8 @@ profit = 0
 profit_past = 0
 firstTimeRunning = True
 count = 0
-sys.stdout.write("CURRENT INVESTMENT,    GAIN/LOSS    |   BTC/USD   |   CHANGES\n")
+sys.stdout.write("CURRENT INVESTMENT, G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES\n\n")
+
 def calcProfits():
 	global firstTimeRunning
 	global profit
@@ -24,46 +26,48 @@ def calcProfits():
 	currDollarBit = float(currDollarBit)    #turning to float to make calculations
 	currDollar = round(myBTC * currDollarBit, 3)
 	profit = round(currDollar - myDollar, 3)
-    
+
 #Calculate Profits:
-	if (profit == 0):  #if im gaining money
-		sys.stdout.write("base: $" +  str(abs(currDollar)) + "   $" +  str(abs(profit)) + "   |   ")
-	elif (profit > 0):  #if im gaining money
-		sys.stdout.write("gain: $" +  str(abs(currDollar)) + "   +$" + str(abs(profit)) + "   |   ")
-	elif (profit < 0):  	#if im losing money
-		sys.stdout.write("loss: $" +  str(abs(currDollar)) + "   -$" + str(abs(profit)) + "   |   ")
-#Calculate My Current Profit:
-	
-#Calculate Percent Change/slope
-	sys.stdout.write("BTC/USD: " + str(currDollarBit) + "   |   ")  #current BTC/USD
+	percent_change = abs(round(abs(profit)/myDollar, 4))
+	if (profit == 0): 
+		
+		sys.stdout.write("base: $" +  str(abs(currDollar)) + "   $" +  str(abs(profit)) + "    " + str(abs(percent_change)) +  "%   |   ")
+	elif (profit > 0):  
+		sys.stdout.write("gain: $" +  str(abs(currDollar)) + "   +$" + str(abs(profit)) + "   +" + str(abs(percent_change)) + "%   |   ")
+	elif (profit < 0):  	
+		sys.stdout.write("loss: $" +  str(abs(currDollar)) + "   -$" + str(abs(profit)) + "   -" + str(abs(percent_change)) +  "%   |   ")	
+
+#Calculate Percent Change/slope  --> Make with constructor next time
+	sys.stdout.write("BTC/USD: " + str(currDollarBit) + "   |   ")
 	if (firstTimeRunning == True):
 		firstTimeRunning = False
 		profit_past = profit
 		print("\nChanged Last Profit")
-	if (profit != profit_past):
-		if (profit > profit_past):
-			diff = abs(round(profit - profit_past, 4))
-			sys.stdout.write("+$" + str(diff) + "    ")    		#print $ +rounded change 
-			percent_change = abs(round(diff/profit_past,4))
-			sys.stdout.write("+" + str(percent_change) + "%\n")	#print % +rounded change
-			
-		if (profit < profit_past):
-			diff = abs(round(profit_past - profit, 4))
-			sys.stdout.write("-$" + str(diff) + "    ")			#print $ -rounded change 
-			percent_change = abs(round(diff/profit_past, 4))
-			sys.stdout.write("-" + str(percent_change) + "%\n")	#print % -rounded change
-	if (profit == profit_past):
-			sys.stdout.write("NO CHANGE" + '\n')			
+	if (profit > profit_past):
+		diff = abs(round(profit - profit_past, 4))
+		percent_change = abs(round(diff/profit_past,4))
+		sys.stdout.write("+$" + str(diff) + "    ")    		#print $ +rounded change 
+		sys.stdout.write("+" + str(percent_change) + "%\n")	#print % +rounded change
+	elif (profit < profit_past):
+		diff = abs(round(profit_past - profit, 4))
+		percent_change = abs(round(diff/profit_past, 4))
+		sys.stdout.write("-$" + str(diff) + "    ")			#print $ -rounded change 
+		sys.stdout.write("-" + str(percent_change) + "%\n")	#print % -rounded change
+	elif (profit == profit_past):
+		sys.stdout.write("NO CHANGE" + '\n')			
+
 #PRINT TO HTML:
 	htmlf = open('profit.html', 'w') #paste the profit onto the profit.html file!
 	#htmlf.write(str(profit))
 	htmlf.close()
+
 #REFRESH:
 	count += 1
 	if (count == 60):       	#after 60 repeats, reset the past profit
 		count = 0;
 		firstTimeRunning = True
-	time.sleep(1)	#program take .5s to execute. so making it refresh at .9s
+	time.sleep(1)	
+
 while True:
 	calcProfits()
 	
