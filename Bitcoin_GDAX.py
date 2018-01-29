37910148f547eeb1b442590d4abcefdc4e9f3d92
Mahmudul Hasan <mhasan0047@gmail.com>
2018-01-29 13:16:22 -0500

    some lines

--- Bitcoin_GDAX.py
+++ Bitcoin_GDAX.py
@@ -4,18 +4,17 @@ import json
 import time		#to refresh scans, repeat function
 import sys		#to print in one line
 
-myBTC = 0#0.01710293 
-myDollar = 0#200#197				
+myBTC = 0		#0.01710293 
+myDollar = 0	#200#197				
 profit = 0
 profit_past = 0
 firstTimeRunning = True
 count = 0
-#MY BITCOIN: bitcoin[USD, BTC, USD/BTC]
+#MY BITCOIN: [USD, BTC, USD/BTC]
 bitcoins = [ [100, 0.00838564, 11805.90], [50, 0.00438647, 11170.71], [50, 0.00433082, 11314.25] ]	 
 for i in range(len(bitcoins)):
 	myDollar = myDollar + bitcoins[i][0] 
 	myBTC = myBTC + bitcoins[i][1] 
-
 
 sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES\n\n")
 def calcProfits():
@@ -36,12 +35,11 @@ def calcProfits():
 #Calculate Profits:
 	percent_change = abs(round(abs(profit)/myDollar*100, 3))
 	if (profit == 0): 
-		
-		sys.stdout.write("$" +  str(abs(currDollar)) + "   |   base:  $" +  str(abs(profit)) + "    " + str(abs(percent_change)) +  "%   |   ")
+		sys.stdout.write("$" +  str(abs(currDollar)) + "  |  base: $" +  str(abs(profit)) + "   " + str(abs(percent_change)) +  "%  |  ")
 	elif (profit > 0):  
-		sys.stdout.write("$" +  str(abs(currDollar)) + "   |   gain:  +$" + str(abs(profit)) + "   +" + str(abs(percent_change)) + "%   |   ")
+		sys.stdout.write("$" +  str(abs(currDollar)) + "  |  gain: +$" + str(abs(profit)) + "  +" + str(abs(percent_change)) + "%  |  ")
 	elif (profit < 0):  	
-		sys.stdout.write("$" +  str(abs(currDollar)) + "   |   loss:  -$" + str(abs(profit)) + "   -" + str(abs(percent_change)) +  "%   |   ")	
+		sys.stdout.write("$" +  str(abs(currDollar)) + "  |  loss: -$" + str(abs(profit)) + "  -" + str(abs(percent_change)) +  "%  |  ")	
 
 #Calculate Percent Change/slope  --> Make with constructor next time
 	sys.stdout.write("BTC/USD: " + str(currDollarBit) + "   |   ")
@@ -53,15 +51,15 @@ def calcProfits():
 		diff = abs(round(profit - profit_past, 3))
 		percent_change = abs(round(diff/profit_past*100,4))
 		sys.stdout.write("+$" + str(diff) + "    ")    		
-		sys.stdout.write("+" + str(percent_change) + "%\n")
+		sys.stdout.write("+" + str(percent_change) + "%")
 	elif (profit < profit_past):
 		diff = abs(round(profit_past - profit, 4))
 		percent_change = abs(round(diff/profit_past, 4))
 		sys.stdout.write("-$" + str(diff) + "    ")			
-		sys.stdout.write("-" + str(percent_change) + "%\n")
+		sys.stdout.write("-" + str(percent_change) + "%")
 	elif (profit == profit_past):
-		sys.stdout.write("NO CHANGE" + '\n')			
-
+		sys.stdout.write("NO CHANGE")			
+	print()
 #PRINT TO HTML:
 	htmlf = open('profit.html', 'w') #paste the profit onto the profit.html file!
 	#htmlf.write(str(profit))
