aedb82ea9189861a4970eb3ef368d698ec7bd0c6
Mahmudul Hasan <mhasan0047@gmail.com>
2018-01-31 00:53:10 -0500

    made working object!

--- Bitcoin_GDAX.py
+++ Bitcoin_GDAX.py
@@ -4,45 +4,44 @@ import json
 import time		
 import sys		
 
-class MyProfits:
+class MyProfits(object):
 	def __init__(self):
 		self.BTC = 0		#total BTC i own
 		self.USD = 0		#total USD i own
 		self.profit = 0
 		self.profit_prev = 0
 firstTimeRunning = True
+total = MyProfits()
 count = 0
 
 #MY BITCOIN: [USD, BTC, USD/BTC]
-market = [ [100, 0.00838564, 11805.90], [50, 0.00438647, 11170.71], [50, 0.00433082, 11314.25], [50, 0.00474367, 10329.55]  ]	 
-
+market = [ [100, 0.00838564, 11805.90], [50, 0.00438647, 11170.71], [50, 0.00433082, 11314.25], [50, 0.00494595, 9907.10]  ]	 
 def calcProfits():
 	t0 = time.time()
 	global firstTimeRunning
 	global count
+	global total
 
-#Calculate BTC and USD for exchanges and total:
-	total = MyProfits()
+	if (firstTimeRunning == True):
+		sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES   |   MY_TOTAL_BTC: "+ str(round(total.BTC,4))+"\n\n")
+#Calculate BTC and USD for exchanges and total:	
 	for i in range(len(market)):
 		total.USD = total.USD + market[i][0] 
 		total.BTC = total.BTC + market[i][1] 
-	"""exchanges = list(range(0,len(market)))  #making an array as long at the elements in the market array
+	exchanges = list(range(0,len(market)))  #making an array as long at the elements in the market array
 	for i in range(len(market)):
-					exchanges[i] = MyProfits()  
-					exchanges[i].USD = exchanges[i].USD + market[i][0] 
-					exchanges[i].BTC = exchanges[i].BTC + market[i][1] """
-	
-	if (firstTimeRunning == True):
-		sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES   |   MY_TOTAL_BTC: "+ str(round(total.BTC,4))+"\n\n")
-#GET CURRENT PRICE OF BITCOIN:
+		exchanges[i] = MyProfits()  
+		exchanges[i].USD = exchanges[i].USD + market[i][0]
+		exchanges[i].BTC = exchanges[i].BTC + market[i][1] 
+	#GET CURRENT PRICE OF BITCOIN:
 	url = 'https://api.gdax.com/products/BTC-USD/trades'
 	res = requests.get(url)
-	json_res = json.loads(res.text) 				#"json_res" got a ton of stuff. the price is in "json_res[0]""
+	json_res = json.loads(res.text) 		#"json_res" got a ton of stuff. the price is in "json_res[0]""
 	currDollarBit = float(json_res[0]['price'])     #current dollars per bitcoin. 'price' is the location in the 0th index of "jason_res" #turning to float to make calculations 
 	currDollar = round(total.BTC * currDollarBit, 3)
 	total.profit = round(currDollar - total.USD, 3)
 
-#Calculate total.profits:
+#Calculate total profits:
 	percent_change = abs(round(abs(total.profit)/total.USD*100, 3))
 	if (total.profit == 0): 
 		sys.stdout.write("$" +  str(abs(currDollar)) + "  |  base: $" +  str(abs(total.profit)) + "   " + str(abs(percent_change)) +  "%  |  ")
@@ -54,13 +53,8 @@ def calcProfits():
 #Calculate Percent Change/slope  --> Make with constructor next time
 	sys.stdout.write("BTC/USD: " + str(currDollarBit) + "   |   ")
 	if (firstTimeRunning == True):
-		firstTimeRunning = False
+		firstTimeRunning = False	
 		total.profit_prev = total.profit
-		print("\nChanged Last total.profit")
-
-	print("\nprof:  "+str(total.profit_prev))
-	print()
-
 	if (total.profit > total.profit_prev):
 		diff = abs(round(total.profit - total.profit_prev, 3))
 		percent_change = abs(round(diff/total.profit_prev*100,4))
@@ -68,9 +62,6 @@ def calcProfits():
 		sys.stdout.write("+" + str(percent_change) + "%")
 	elif (total.profit < total.profit_prev):
 		diff = abs(round(total.profit_prev - total.profit, 3))
-		print(diff)
-		print()
-
 		percent_change = abs(round(diff/total.profit_prev*100, 4))
 		sys.stdout.write("-$" + str(diff) + "    ")			
 		sys.stdout.write("-" + str(percent_change) + "%")
@@ -82,14 +73,16 @@ def calcProfits():
 	#htmlf.write(str(total.profit))
 	htmlf.close()
 
-#REFRESH:
+#REFRESH+ENDING:
 	count += 1
+	t1 = time.time()
+	total_time = round((t1-t0),3)
+	sys.stdout.write("    |  " + str(total_time)+" sec        " + str(count))
 	if (count == 60):       	
 		count = 0;
 		firstTimeRunning = True
-	t1 = time.time()
-	total = round((t1-t0),3)
-	sys.stdout.write("    |  " + str(total)+" sec")
+	total.USD = 0   #reseting calculations 
+	total.BTC = 0
 	print()
 	time.sleep(.6)	
 
