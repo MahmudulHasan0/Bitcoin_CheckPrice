b1d8c2b770695c1d9bd07d1849e0d7b54418f985
Mahmudul Hasan <mhasan0047@gmail.com>
2018-02-01 23:22:16 -0500

    deleated some files

--- Bitcoin_GDAX.py
+++ Bitcoin_GDAX.py
@@ -1,75 +1,74 @@
-#Total Bitcoin Wallet Checker
+#TOTAL BTC WALLET CHECKER
 #Next Strategies: python write to a file. get html to read from that file and refresh every second
 import requests
 import json
 import time		
 import sys		
 
-class MyProfits(object):
+class MyProfits():
 	def __init__(self):
-		self.BTC = 0		#total BTC i own
+		self.BTC = 0		#total btc i own
 		self.USD = 0		#total USD i own
 		self.currDollar = 0
-		self.profit = 0
-		self.profit_prev = 0
+		self.change = 0
+		self.change_prev = 0
 firstTimeRunning = True
 total = MyProfits()
 count = 0
 
-#MY BITCOIN: [USD, BTC, USD/BTC]
+#INPUTING MY BITCOIN EXCHANGES: [USD, BTC, USD/BTC]
 bought = [ [100, 0.00838564, 11805.90], [50, 0.00438647, 11170.71], [50, 0.00433082, 11314.25], [50, 0.00494595, 9907.10], [0, 0.001006, 10154], [50, 0.00526249, 9311.18]]	 
 def calcProfits():
 	t0 = time.time()
 	global firstTimeRunning
 	global count
 	global total
-#Calculate BTC and USD for my total profits:
-	#Tell me the total bitcoin and USD i currently have in this market:
+#CALCULATE TOTAL NUMBER OF BITCOINS I OWN AND THE TOTAL USD I SPEND ON THEM:
 	for i in range(len(bought)):
 		total.USD = total.USD + bought[i][0] 
 		total.BTC = total.BTC + bought[i][1] 
-#Print what i have right now 
+#PRINT WHAT I HAVE RIGHT NOW
 	if (firstTimeRunning == True):
 		sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES   |   USD: "+ str(round(total.USD,3)) + "   BTC: "+ str(round(total.BTC,7))+"\n\n")
 #GET CURRENT PRICE OF BITCOIN:
 	url = 'https://api.gdax.com/products/BTC-USD/trades'
 	res = requests.get(url)
-	json_res = json.loads(res.text) 				#"json_res" got a ton of stuff. the price is in "json_res[0]""
+	json_res = json.loads(res.text) 					  #"json_res" got a ton of stuff. the price is in "json_res[0]""
 	total.currDollarBit = float(json_res[0]['price'])     #current dollars per bitcoin. 'price' is the location in the 0th index of "jason_res" #turning to float to make calculations 
-	total.currDollar = round(total.BTC * total.currDollarBit, 3)
-	total.profit = round(total.currDollar - total.USD, 3)
-#Calculate total profits:
-	percent_change = abs(round(abs(total.profit)/total.USD*100, 3))
-	if (total.profit == 0): 
-		sys.stdout.write("$" +  str(abs(total.currDollar)) + "  |  base: $" +  str(abs(total.profit)) + " " + str(abs(percent_change)) +  "%   ")
-	elif (total.profit > 0):  
-		sys.stdout.write("$" +  str(abs(total.currDollar)) + "  |  gain:+$" + str(abs(total.profit)) + " +" + str(abs(percent_change)) + "%  |  ")
-	elif (total.profit < 0):  	
-		sys.stdout.write("$" +  str(abs(total.currDollar)) + "  |  loss:-$" + str(abs(total.profit)) + " -" + str(abs(percent_change)) +  "%  |  ")	
-#Calculate Percent Change/slope  
+#CALCULATE THE TOTAL CHANGES 
+	total.currDollar = round(total.BTC * total.currDollarBit, 3) #the total usd in bitcoins i have currently
+	total.change = round(total.currDollar - total.USD, 3)
+	percent_change = abs(round(abs(total.change)/total.USD*100, 3))
+	if (total.change == 0): 
+		sys.stdout.write("$" +  str(abs(total.currDollar)) + "  |  base: $" +  str(abs(total.change)) + " " + str(abs(percent_change)) +  "%  ")
+	elif (total.change > 0):  
+		sys.stdout.write("$" +  str(abs(total.currDollar)) + "  |  gain:+$" + str(abs(total.change)) + " +" + str(abs(percent_change)) + "%  |  ")
+	elif (total.change < 0):  	
+		sys.stdout.write("$" +  str(abs(total.currDollar)) + "  |  loss:-$" + str(abs(total.change)) + " -" + str(abs(percent_change)) +  "%  |  ")	
+#Calculate Percent Change SINCE 60TH READING
 	sys.stdout.write("BTC/USD: " + str(total.currDollarBit) + "   |  ")
 	if (firstTimeRunning == True):
 		firstTimeRunning = False	
-		total.profit_prev = total.profit
-	if (total.profit > total.profit_prev):
-		diff = abs(round(total.profit - total.profit_prev, 3))
-		percent_change = abs(round(diff/total.profit_prev*100,4))
-		sys.stdout.write("+$" + str(diff) + "    ")    		
+		total.change_prev = total.change
+	if (total.change > total.change_prev):
+		diff = abs(round(total.change - total.change_prev, 3))
+		percent_change = abs(round(diff/total.change_prev*100,2))
+		sys.stdout.write("+$" + str(diff) + "  ")    		
 		sys.stdout.write("+" + str(percent_change) + "%")
-	elif (total.profit < total.profit_prev):
-		diff = abs(round(total.profit_prev - total.profit, 3))
-		percent_change = abs(round(diff/total.profit_prev*100, 4))
-		sys.stdout.write("-$" + str(diff) + "    ")			
+	elif (total.change < total.change_prev):
+		diff = abs(round(total.change_prev - total.change, 3))
+		percent_change = abs(round(diff/total.change_prev*100, 2))
+		sys.stdout.write("-$" + str(diff) + "  ")			
 		sys.stdout.write("-" + str(percent_change) + "%")
-	elif (total.profit == total.profit_prev):
+	elif (total.change == total.change_prev):
 		sys.stdout.write("NO CHANGE")			
 #PRINT TO HTML:
-	htmlf = open('profit.html', 'w') #paste the total.profit onto the total.profit.html file!
-	#htmlf.write(str(profit))
-	htmlf.close()
-
+	#htmlf = open('change.html', 'w') #paste the total.change onto the total.change.html file!
+	#htmlf.write(str(change))
+	#htmlf.close()
+	
 #REFRESH+ENDING:
-	total.USD = 0   #reseting calculations 
+	total.USD = 0   
 	total.BTC = 0
 	count += 1
 	if (count == 60):       	
