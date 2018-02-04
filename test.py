a5917371e72de926e1ada803d45f7730662e75a1
Mahmudul Hasan <mhasan0047@gmail.com>
2018-02-04 01:02:48 -0500

    t

--- test.py
+++ test.py
@@ -10,11 +10,14 @@ class MyProfits():
 	def __init__(self):
 		self.USD = 0 		
 		self.BTC = 0		
+		self.buyUSD = 0
+		self.buyBTC = 0
+		self.soldUSD = 0
+		self.sellBTC = 0		
+
 		self.currDollar = 0
 		self.change = 0
 		self.change_prev = 0
-		self.soldUSD = 0
-		self.soldBTC = 0
 firstTimeRunning = True
 inPut = MyProfits()		#USD and BTC i entered
 current = MyProfits()	#USD and BTC in my wallet right now
@@ -25,11 +28,9 @@ y = 0
 #INPUTING MY BITCOIN EXCHANGES: BOUGHT = [USD, BTC, USD/BTC]. SOLD = [USD, BTC]
 #bought = [ [-100, 0.00838564, 11805.90], [-50, 0.00438647, 11170.71], [-50, 0.00433082, 11314.25], [-50, 0.00494595, 9907.10], [0, 0.001006, 10154], [-50, 0.00526249, 9311.18], [-50,0.00579132, 8460.94] ]	 
 #With cointotal $1 free:
-bought = [
-	[0.00838564, 11805.9, -99], [0.00433082, 11314.25, -49], [0.00438647, 11170.71, -49], [0.00494595, 9907.1, -49], [0.00526249, 9311.18, -49], [0.00579132, 8460.94, -49], [0.00511541, 9750.01, -50], [0.001006, 10154, 0]
-    ,[0.00449406, 8523.29, -38.4], [0.00577656, 8565.01, -49.6], [0.00290492, 9374.96], [.003,9375], [0.00541882, 9147.01], [0.00539200, 9100.01], [0.00539200, 9100.01], [0.00536428, 9217.71]
-]
+bought = [[0.00838564, 11805.9, -99], [0.00433082, 11314.25, -49], [0.00438647, 11170.71, -49], [0.00494595, 9907.1, -49], [0.00526249, 9311.18, -49], [0.00579132, 8460.94, -49], [0.00511541, 9750.01, -50], [0.001006, 10154, 0]]
 #0:BTC BOUGHT 	#1:ratio USD/BTC	#2:usd input for MEMORYv
+buy = [[0.00449406, 8523.29, -38.4], [0.00577656, 8565.01, -49.6], [0.00290492, 9374.96], [.003,9375], [0.00541882, 9147.01], [0.00539200, 9100.01], [0.00539200, 9100.01], [0.00536428, 9217.71]]
 sold   = [[-0.00438647, 8777.94, 38.4], [-.002,8585.00], [-0.00279132, 8585.00], [-0.00577656, 8875.01], [-0.00544134, 9155.00], [-0.00541882, 9100.00], [-0.00539200, 9217.75], [0.00100000, 9205.00], [0.00253276, 9204.62], [0.00183152,9202.94] ]    
 #0: BTC sold 	#1: ratio USD/BTC 	#2: USD gained for memory
 fee = .002494 #GDAX Transaction Fee Percentage
@@ -38,17 +39,16 @@ u=0
 for i in range(len(bought)):
 	inPut.USD = inPut.USD + bought[i][2] 
 	inPut.BTC = inPut.BTC + bought[i][0]
-	if (bought[i][2] == 0):
-		break
 for i in range(len(bought)):
 	total.BTC = total.BTC + bought[i][0] #will multiply this by the ratio of USD/BTC to get USD
 	x = x + bought[i][0]
 
+for i in range(len(buy)):
+	total.BTC = total.BTC + buy[i][0] #will multiply this by the ratio of USD/BTC to get USD
+	total.buyBTC = total.buyBTC + buy[i][0]
+
 for i in range(len(sold)):
-	total.soldBTC = total.soldBTC + sold[i][0]  #will divide this by the ratio of USD/BTC to get USD
-	c = sold[i][0] - (sold[i][0]*fee)
-	u = u + c
-	y = y + sold[i][0]
+	total.sellBTC = total.sellBTC + (-sold[i][0])  #will divide this by the ratio of USD/BTC to get USD
 
 
 def calcProfits():
@@ -61,18 +61,24 @@ def calcProfits():
 	current.dollarBit = float(json_res[0]['price'])     # current dollars per bitcoin. 'price' is the location in the 0th index of "jason_res" #turning to float to make calculations 
 
 #CALCULATE current NUMBER OF BTC I OWN AND THE current USDed I SPEND ON THEM + SOLD BTC:
+	p = total.buyBTC*current.dollarBit +total.sellBTC*current.dollarBit
+	print("ssssssss:")
+	print(total.buyBTC*current.dollarBit )
+	print(total.sellBTC*current.dollarBit )
+	print(total.sellBTC*current.dollarBit - total.buyBTC*current.dollarBit )
+	print()
 
-	soldUSD = round(-total.soldBTC*current.dollarBit,3)
+	soldUSD = round(-total.sellBTC*current.dollarBit,3)
 	soldUSD = round(soldUSD - (soldUSD*fee),4) #When i sold my bitcoins, how much USD did i get back? Applying the fee because from BTC to USD a fee is applied
-	soldBTC = round(total.soldBTC,8)
+	sellBTC = round(total.sellBTC,8)
 
 	current.USD = round(-total.BTC*current.dollarBit,3) #From USD to BTC, a fee is applied. Here, im directly converting BTC to USD so no need to apply the fee
 	current.BTC = round(total.BTC,8)
 	#current.USD = (current.USD + soldUSD )
-	#current.BTC = (current.BTC + soldBTC)
+	#current.BTC = (current.BTC + sellBTC)
 	z = (-x*current.dollarBit)+(-y*current.dollarBit)
 
-	print(total.soldBTC*fee*current.dollarBit)
+	print(total.sellBTC*fee*current.dollarBit)
 	print(u)
 	print(current.USD)
 	print(soldUSD)
@@ -84,7 +90,7 @@ def calcProfits():
 
 #PRINT THE current I HAVE INPUT TO SYSTEM (BOUGHT), AND current I HAVE SOLD
 	if (firstTimeRunning == True):
-		sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES   ||INPUT:   USD:"+ str(round(inPut.USD,3)) + "   BTC:+"+ str(round(inPut.BTC,9))+"   ||SOLD:   USD:+"+str(soldUSD) + "   BTC:"+str(soldBTC)+"\n\n")
+		sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES   ||INPUT:   USD:"+ str(round(inPut.USD,3)) + "   BTC:+"+ str(round(inPut.BTC,9))+"   ||SOLD:   USD:+"+str(soldUSD) + "   BTC:"+str(sellBTC)+"\n\n")
 #PRINT THE USD AND BTC I HAVE IN MY WALLET RIGHT NOW:	
 	if (firstTimeRunning == True):
 		sys.stdout.write("REMAINING IN WALLET: USD:    $" + str(current.USD) + "   BTC: " + str(round(current.BTC,9))+"\n\n")
