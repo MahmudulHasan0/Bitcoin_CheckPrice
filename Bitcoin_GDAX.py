35ed7accb964dcd2ab96b570b4b18f07b6da75e1
Mahmudul Hasan <mhasan0047@gmail.com>
2018-02-04 00:34:53 -0500

    make a buy array

--- Bitcoin_GDAX.py
+++ Bitcoin_GDAX.py
@@ -23,8 +23,18 @@ soldBTC = 0
 #INPUTING MY BITCOIN EXCHANGES: BOUGHT = [USD, BTC, USD/BTC]. SOLD = [USD, BTC]
 #bought = [ [-100, 0.00838564, 11805.90], [-50, 0.00438647, 11170.71], [-50, 0.00433082, 11314.25], [-50, 0.00494595, 9907.10], [0, 0.001006, 10154], [-50, 0.00526249, 9311.18], [-50,0.00579132, 8460.94] ]	 
 #With coinbase $1 free:
-bought = [ [-99, 0.00838564, 11805.90], [-49, 0.00433082, 11314.25], [-49, 0.00438647, 11170.71], [-49, 0.00494595, 9907.10],  [-49, 0.00526249, 9311.18], [-49,0.00579132, 8460.94], [0, 0.001006, 10154], [-38.40, 0.00449406, 8523.29], [-49.6, 0.00577656, 8565.01] ]#x	  #BTC Important   #[lower,greater, lower]   #official 
-sold   = [ [38.40, -.00438647, 8777.94], [49.6,-0.00579132, 8585.00], [51.14,-0.00577656, 8875.01]]	 #USD IMPORTANT  #[greater,lower,greater]		 #sell when higher ratio   #sold my BTC (-) for USD (+)
+
+#0:usd input
+#1:usd/btc
+#2:sold btc
+bought = [ [-99, 11805.90, 0.00838564], [-49, 11314.25, 0.00433082], [-49, 11170.71, 0.00438647], [-49, 9907.10, 0.00494595],  [-49, 9311.18, 0.00526249], [-49,8460.94, 0.00579132], [0, 10154, 0.001006]]#, [-38.40, 8523.29], [-49.6, 8565.01], [-51.14, 9375] ]#x	  #BTC Important   #[lower,greater, lower]   #official 
+
+#0: BTC sold
+#1: BTC/USD
+#3, 
+sold   = [[0,0]]#[ [38.40, -.00438647, 8777.94], [49.6,-0.00579132, 8585.00], [51.14,-0.00577656, 8875.01]]	 #USD IMPORTANT  #[greater,lower,greater]		 #sell when higher ratio   #sold my BTC (-) for USD (+)
+
+
 """
 SELL WATCHLIST
 11805.90
@@ -41,33 +51,37 @@ BUY WATCHLIST
 8585.00 X
 8777.94 x
 """
+
+#MAKING BASE CALCULATIONS:
 for i in range(len(bought)):
 	total.USD_input = total.USD_input + bought[i][0] 
 	total.BTC_input = total.BTC_input + bought[i][1]
 	if (bought[i][0] == 0):
 		break
+for i in range(len(sold)):
+	soldUSD = sold[i][0]
+	soldBTC = sold[i][1]
+
 def calcProfits():
 	t0 = time.time()
 	global firstTimeRunning, count, total, soldUSD, soldBTC
 #CALCULATE TOTAL NUMBER OF BTC I OWN AND THE TOTAL USDed I SPEND ON THEM + SOLD BTC:
-	for i in range(len(sold)):
-		soldUSD = sold[i][0]
-		soldBTC = sold[i][1]
 	for i in range(len(bought)):
 		total.USD = total.USD + bought[i][0] 
 		total.BTC = total.BTC + bought[i][1]
-	total.USD = total.USD_input - (total.USD + soldUSD )
-	total.BTC = total.BTC_input - (total.BTC + soldBTC)
+	total.USD= (total.USD + soldUSD )
+	total.BTC = (total.BTC + soldBTC)
+
+	print()
+	sys.stdout.write("net USD :  "+str(total.USD))
 
 #PRINT THE TOTAL I HAVE INPUT TO SYSTEM (BOUGHT), AND TOTAL I HAVE SOLD
 	if (firstTimeRunning == True):
 		sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES   ||INPUT:   USD:"+ str(round(total.USD_input,3)) + "   BTC:+"+ str(round(total.BTC_input,9))+"   ||SOLD:   USD:+"+str(soldUSD) + "   BTC:"+str(soldBTC)+"\n\n")
 #PRINT THE USD AND BTC I HAVE IN MY WALLET RIGHT NOW:	
-	
 	if (firstTimeRunning == True):
 		sys.stdout.write("REMAINING IN WALLET: USD:    $" + str(total.USD) + "   BTC: " + str(round(total.BTC,9))+"\n\n")
 		sys.stdout.write("CURRENT: \n")
-
 #GET CURRENT PRICE OF BITCOIN:
 	url = 'https://api.gdax.com/products/BTC-USD/trades'
 	res = requests.get(url)
@@ -75,6 +89,13 @@ def calcProfits():
 	total.currDollarBit = float(json_res[0]['price'])     # current dollars per bitcoin. 'price' is the location in the 0th index of "jason_res" #turning to float to make calculations 
 #CALCULATE THE TOTAL CHANGES 
 	total.currDollar = round(total.BTC * total.currDollarBit, 3) # the total usd in bitcoins i have currently
+	
+	sys.stdout.write("\ncurrDollar:  "+str(total.currDollar))
+	print()
+	sys.stdout.write("total change_2:  "+str(total.currDollar))
+	print()
+	print()
+
 	total.change = round(total.currDollar + total.USD, 3)
 	if (total.USD == 0): 								
 		percent_change = 0
@@ -117,7 +138,7 @@ def calcProfits():
 		firstTimeRunning = True
 	t1 = time.time()
 	total_time = round((t1-t0),3)
-	sys.stdout.write("  |            " + str(total_time)+" sec        " + str(count) + "\n")
+	sys.stdout.write("  |            " + str(total_time)+" sec        " + str(count) + "\n---------------------------------------------------------------------------\n")
 	time.sleep(1)	
 
 while True:
