e0d8204377d1f9b5ca0e89b8c10a990b3bd2920d
Mahmudul Hasan <mhasan0047@gmail.com>
2018-02-11 15:25:40 -0500

    simplify

--- Bitcoin_Total.py
+++ Bitcoin_Total.py
@@ -4,6 +4,11 @@ import json
 import time		
 import sys		
 
+#INPUTING MY BITCOIN EXCHANGES: investment = [USD, BTC, USD/BTC]
+investment = [ [-100, 0.00838564, 11805.90], [-50, 0.00433082, 11314.25], [-50, 0.00438647, 11170.71], [-50, 0.00494595, 9907.10], [-50, 0.00526249, 9311.18], [-50, .00579132, 8460.94], [-50, 0.00723745, 6605.92], [0, 0.001006, 10154], [-50, 0.00511541, 9750.01] ]
+sold = [[0,0,0]]						 
+myCurrentBTC = 0.04416541
+
 class MyProfits():
 	def __init__(self):
 		self.USD = 0 			
@@ -20,9 +25,7 @@ each = MyProfits()		#object for each investment
 count = 0
 soldUSD = 0
 soldBTC = 0
-#inputING MY BITCOIN EXCHANGES: investment = [USD, BTC, USD/BTC]. SOLD = [USD, BTC]
-investment = [ [-100, 0.00838564, 11805.90], [-50, 0.00433082, 11314.25], [-50, 0.00438647, 11170.71], [-50, 0.00494595, 9907.10], [-50, 0.00526249, 9311.18], [-50, .00579132, 8460.94], [-50, 0.00723745, 6605.92], [0, 0.001006, 10154], [-50, 0.00511541, 9750.01] ]
-sold = [[0,0,0]] #ignore this array
+
 for i in range(len(investment)):
 	input.USD = input.USD + investment[i][0] 
 	input.BTC = input.BTC + investment[i][1]
@@ -32,8 +35,7 @@ for i in range(len(sold)):
 
 def calcProfits():
 	t0 = time.time()
-
-	global firstTimeRunning, count, input, current, each, soldUSD, soldBTC
+	global firstTimeRunning, count, input, current, each, soldUSD, soldBTC, myCurrentBTC
 #1) PRINT THE input I HAVE input TO SYSTEM (investment), AND input I HAVE SOLD
 	if (firstTimeRunning == True):
 		sys.stdout.write("CURRENT INVESTMENT   |    G/L DOLLARS, G/L PERCENTAGE   |   BTC/USD   |   PERCENT CHANGES   ||input:   USD:"+ str(round(input.USD,3)) + "   BTC:+"+ str(round(input.BTC,9))+"   ||SOLD:   USD:+"+str(soldUSD) + "   BTC:"+str(soldBTC)+"\n\n")
@@ -42,14 +44,14 @@ def calcProfits():
 	url = 'https://api.gdax.com/products/BTC-USD/trades'
 	res = requests.get(url)
 	json_res = json.loads(res.text) 					 
-	current.dollarBit = float(json_res[0]['price'])     
+	current.dollarBit = float(json_res[0]['price'])   
 
 #3) CALCULATE CURRENT USD AND BTC AND CURRENT LOSSES/GAINS:
 	for i in range(len(investment)):
 		each.USD = investment[i][1]*current.dollarBit
-		each.gainLossArray.append(investment[i][0]+each.USD) #CALCULATE THE GAIN/LOSS OF EACH INVESTMENT, AND PUT IT IN ARRAY (WILL BE USED TO EVALUATES GAINS/LOSSES OF EACH INVESTMENT)
+		each.gainLossArray.append(investment[i][0]+each.USD) 	#CALCULATE THE GAIN/LOSS OF EACH INVESTMENT, AND PUT IT IN ARRAY (WILL BE USED TO EVALUATES GAINS/LOSSES OF EACH INVESTMENT)
 		input.gainLoss = input.gainLoss + each.gainLossArray[i] #gain loss using the inputs
-	current.BTC = 0.04416541#input.BTC
+	current.BTC = myCurrentBTC
 	current.USD = round(current.BTC * current.dollarBit,3)
 	current.gainLoss = round(input.USD + current.USD,3)
 	current.gainLossPercent = round(current.gainLoss/450*100,3)
@@ -69,7 +71,6 @@ def calcProfits():
 
 #Calculate Percent Change SINCE 60TH READING
 	sys.stdout.write("BTC/USD: " + str(current.dollarBit) + "   |  ")
-
 	if (firstTimeRunning == True):
 		firstTimeRunning = False	
 		current.gainLoss_prev = current.gainLoss
@@ -85,17 +86,14 @@ def calcProfits():
 		sys.stdout.write("-" + str(current.gainLossPercent) + "%")
 	elif (current.gainLoss == current.gainLoss_prev):
 		sys.stdout.write("NO CHANGE")			
-
 	current.USD = 0
 	current.BTC = 0
 	current.gainLoss = 0
 	current.gainLossPercent = 0
-
 	count += 1
 	if (count == 60):
 		count = 0;
 		firstTimeRunning = True
-
 	t1 = time.time()
 	input_time = round((t1-t0),3)
 	sys.stdout.write("  |            " + str(input_time)+" sec        " + str(count) + "\n")
