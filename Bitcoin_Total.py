6174f71013c4b44699e7b2940debaba220e54781
Mahmudul Hasan <mhasan0047@gmail.com>
2018-02-15 18:49:57 -0500

    not much chnage

--- Bitcoin_Total.py
+++ Bitcoin_Total.py
@@ -4,36 +4,36 @@ import json
 import time		
 import sys		
 
-#INPUTING MY BITCOIN EXCHANGES: investment = [BTCtoUSD, BTC, BTCtoUSD/BTC]
+#INPUTING MY EXCHANGES: investment = [USD input, BTC, USD/BTC]
 investment = [ [100, 0.00838564, 11805.90], [50, 0.00433082, 11314.25], [50, 0.00438647, 11170.71], [50, 0.00494595, 9907.10], [50, 0.00526249, 9311.18], [50, .00579132, 8460.94], [50, 0.00723745, 6605.92], [0, 0.001006, 10154], [50, 0.00511541, 9750.01] ]					 
-inWallet = [51.6, (0.02204888+0.01439611)]  #What I have in my wallets right now:  [current BTCtoUSD from selling BTC, current BTC]
+inWallet = [51.6, (0.02204888+0.01435868)]  #What I have in my wallets right now:  [current BTCtoUSD from selling BTC, current BTC]
 
 class MyProfits():
 	def __init__(self):
 		self.BTC = 0
-		self.BTCtoUSD = 0 	#$ in BTC
-		self.totalUSD = 0	#everything converted to USD
+		self.BTCtoUSD = 0 	#Dollars in BTC
+		self.totalUSD = 0	#Everything converted to USD
 		self.dollarBit = 0
 		self.gainLoss = 0
 		self.gainLossArray = []
 		self.gainLossPercent = 0
 		self.gainLoss_prev = 0
 firstTimeRunning = True
-input = MyProfits()		#object for all initial investent. BTCtoUSD i inputted to system
-current = MyProfits()	#current BTCtoUSD, BTC i have with the market
-each = MyProfits()		#object for each investment
+input = MyProfits()		#Object for all initial investent.
+current = MyProfits()	#Current BTCtoUSD, BTC i have with the market
+each = MyProfits()		#Object for each investment
 count = 0
-
 for i in range(len(investment)):
 	input.totalUSD = input.totalUSD + investment[i][0] 
 	input.BTC = input.BTC + investment[i][1]
+
 
 def calcProfits():
 	t0 = time.time()
 	global firstTimeRunning, count, input, current, each, inWallet
 #1) PRINT THE input I HAVE input TO SYSTEM (investment), AND input I HAVE SOLD
 	if (firstTimeRunning == True):
-		sys.stdout.write("CURRENT MONEY   |    G/L DOLLARS, G/L PERCENTAGE   |   USD/BTC   |   PERCENT CHANGES   |   INITIAL:  USD: $"+ str(round(input.totalUSD,3)) + "   BTC:"+ str(round(input.BTC,9))+"\n\n")
+		sys.stdout.write("CURRENT MONEY   |    G/L DOLLARS, G/L PERCENTAGE   |   USD/BTC   |   PERCENT CHANGES   |   INITIAL:  INPUT-USD: $"+ str(round(input.totalUSD,3)) + "   GOTTEN-BTC: "+ str(round(input.BTC,9))+"\n\n")
 #2) GET CURRENT PRICE OF BITCOIN:
 	url = 'https://api.gdax.com/products/BTC-USD/trades'
 	res = requests.get(url)
@@ -45,7 +45,7 @@ def calcProfits():
 		each.gainLossArray.append(investment[i][0]-each.BTCtoUSD) 	#CALCULATE THE GAIN/LOSS OF EACH INVESTMENT, AND PUT IT IN ARRAY (WILL BE USED TO EVALUATES GAINS/LOSSES OF EACH INVESTMENT)
 		input.gainLoss = input.gainLoss + each.gainLossArray[i] 	#Gain/loss using the inputs
 	current.BTCtoUSD = round(inWallet[1] * current.dollarBit,3)		#Turning the BTC to USD
-	current.totalUSD = round(inWallet[0] + current.BTCtoUSD,3) 				#Basically turning everything in wallet to USD
+	current.totalUSD = round(inWallet[0] + current.BTCtoUSD,3) 		#Basically turning everything in wallet to USD
 	current.gainLoss = -round(input.totalUSD - current.totalUSD,3)
 	current.gainLossPercent = round(current.gainLoss/input.totalUSD*100,3)
 #4) PRINT THE BTCtoUSD AND BTC I HAVE IN MY WALLET RIGHT NOW:	
